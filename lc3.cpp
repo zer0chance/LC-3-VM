bb33eba008d691229e99b65cc4dd2dc1f3783e3c
zer0chance <ier7928@gmail.com>
2019-08-11 15:13:14 +0700

    Fetch TRAP opcode

--- lc3.cpp
+++ lc3.cpp
@@ -55,6 +55,15 @@ enum
     FL_NEG = 1 << 2, /* N */
 };
 
+enum
+{
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
 
 
 uint16_t memory[UINT16_MAX];
@@ -280,7 +289,67 @@ int main(int argc, char const *argv[])
                 break;
 
             case OP_TRAP:
-                {TRAP, 8}
+                {
+                	switch (instr & 0xFF)
+					{
+					    case TRAP_GETC:
+					        {
+					        	reg[R_R0] = (uint16_t)getchar();
+					        }	
+					        break;
+
+					    case TRAP_OUT:
+					        {	
+					        	putc((char)reg[R_R0], stdout);
+								fflush(stdout);
+							}
+					        break;
+
+					    case TRAP_PUTS:
+					        {							    
+							    uint16_t* c = memory + reg[R_R0];
+							    while (*c)
+							    {
+							        putc((char)*c, stdout);
+							        ++c;
+							    }
+							    fflush(stdout);
+							}
+					        break;
+
+					    case TRAP_IN:
+					        {
+					        	printf("Enter a character: ");
+								char c = getchar();
+								putc(c, stdout);
+								reg[R_R0] = (uint16_t)c;
+					        }
+					        break;
+
+					    case TRAP_PUTSP:
+					        {
+							    uint16_t* c = memory + reg[R_R0];
+							    while (*c)
+							    {
+							        char char1 = (*c) & 0xFF;
+							        putc(char1, stdout);
+							        char char2 = (*c) >> 8;
+							        if (char2) putc(char2, stdout);
+							        ++c;
+							    }
+							    fflush(stdout);
+							}
+					        break;
+
+					    case TRAP_HALT:
+					        {
+					        	puts("HALT");
+								fflush(stdout);
+								running = 0;
+							}	
+					        break;
+					}
+                }
                 break;
 
             case OP_RES:
