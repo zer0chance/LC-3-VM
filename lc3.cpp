63eb4925134e7b2d129c844a0cfeeed294adf8e4
zer0chance <ier7928@gmail.com>
2019-08-11 15:22:05 +0700

    Create functions to load programs

--- lc3.cpp
+++ lc3.cpp
@@ -65,6 +65,12 @@ enum
     TRAP_HALT = 0x25   /* halt the program */
 };
 
+enum
+{
+    MR_KBSR = 0xFE00, /* keyboard status */
+    MR_KBDR = 0xFE02  /* keyboard data */
+};
+
 
 uint16_t memory[UINT16_MAX];
 
@@ -89,6 +95,46 @@ void update_flags(uint16_t r)
 		reg[R_COND] = FL_NEG;
 
 	else reg[R_COND] = FL_POS;
+}
+
+
+
+uint16_t swap16(uint16_t x)
+{
+    return (x << 8) | (x >> 8);
+}
+
+
+
+void read_image_file(FILE* file)
+{
+    /* the origin tells us where in memory to place the image */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    /* we know the maximum file size so we only need one fread */
+    uint16_t max_read = UINT16_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    /* swap to little endian */
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+
+
+int read_image(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) { return 0; };
+    read_image_file(file);
+    fclose(file);
+    return 1;
 }
 
 
